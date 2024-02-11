0176ad60638081e9461f8c0315cb652b0b083c67
Manny <mannyg2600@gmail.com>
2024-02-11 16:47:08 -0500

    first commit

--- script.js
+++ script.js
@@ -1,2 +1,41 @@
-console.log("hello")
-alert('slippery n-word pass')
+// console.log("hello")
+
+
+// var country = "Sololand"
+// var isisland = "true"
+// var population = 660
+// var newpop = population + 1
+// var halfpop = population/2
+// var avgpop = 33000000
+
+// var language = "English"
+// var description = ('Sololand is in Antartica, and its' + population + " people speaks " + language)
+
+
+// if (population > avgpop){
+//     console.log("Your population is bigger tham the average population.")
+
+// }else{
+//     console.log('Your population is smaller tham the average population.')
+// }
+// console.log(country)
+// console.log(isisland)
+// console.log(population)
+// console.log(language)
+// console.log(description)
+// console.log(`My country is ${country}`, `${isisland+population}`)
+// if (population < avgpop){
+//     console.log(`Your population is ${avgpop - population}`)
+// }
+
+
+var country = "Sololand"
+var isisland = "false"
+var population = 6600
+var lngauge = "English"
+
+if (isisland = "false", language = "English", isisland = "false"){
+    console.log("You should consider Sololand")
+}else{
+    console.log("Sololand dont want u.")
+}
